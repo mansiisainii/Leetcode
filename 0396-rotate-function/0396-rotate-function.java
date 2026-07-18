5d347b83e7f9ad106443b8d433d63840fd5932f2
Mansi <mansiisaini29@gmail.com>
2026-07-18 07:33:08 +0530

    Time: 4 ms (94.08%), Space: 91.2 MB (12.6%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        long sum = 0;
+        long f = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            f += (long) i * nums[i];
+        }
+        long ans = f;
+        for (int i = n - 1; i > 0; i--) {
+            f = f + sum - (long) n * nums[i];
+            ans = Math.max(ans, f);
+        }
+        return (int) ans;
+    }
+}
